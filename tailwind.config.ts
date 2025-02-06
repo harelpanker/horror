b5b35a5a60a07a4672587d394e57a6dccfbcde6f
Harel Panker <harelpanker@gmail.com>
2025-02-06 18:36:33 +0100

    change font to terminal font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,7 +16,7 @@ export default {
 				info: 'var(--info)',
 			},
 			fontFamily: {
-				spaceGrotesk: ['var(--font-space-grotesk)'],
+				inconsolata: ['var(--font-inconsolata)'],
 			},
 			boxShadow: {
 				input: `0px 2px 3px -1px rgba(0,0,0,0.1), 0px 1px 0px 0px rgba(25,28,33,0.02), 0px 0px 0px 1px rgba(25,28,33,0.08)`,
